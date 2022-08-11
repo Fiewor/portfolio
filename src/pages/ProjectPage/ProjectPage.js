6f233d806829b319d0220e56c033b793649349af
Fiewor <johnfiewor@gmail.com>
2022-08-11 14:47:37 +0100

    feat: add tags and refactor project tile

--- src/pages/ProjectPage/ProjectPage.js
+++ src/pages/ProjectPage/ProjectPage.js
@@ -4,6 +4,8 @@ import { Grid, Column, Button } from "@carbon/react";
 import starCard from "../../assets/star-card.PNG";
 import aiGrader from "../../assets/ai-grader.png";
 import goodNews from "../../assets/good-news.PNG";
+import mollo from "../../assets/mollo.PNG";
+import anyLease from "../../assets/anylease.PNG";
 
 const ProjectPage = () => {
   // ? Which projects do I want to display:
@@ -14,45 +16,73 @@ const ProjectPage = () => {
     {
       id: "1",
       name: "AI-Grader",
+      description:
+        "Using Azure AI Cognitive services to grade handwritten PBTs.",
       image: aiGrader,
       live: "https://www.aigrader.live",
       repo: "https://github.com/Fiewor/ai-grader",
       alt: "ai-grader landing page",
+      tags: [{ red: "Undergoing fixes" }, { green: "Live" }],
     },
     {
       id: "2",
       name: "GoodNews",
+      description:
+        "Web application displaying positive news from over 100 countries using GDELT dataset.",
       image: goodNews,
       live: "https://github.com/avik-singha/good-news",
       repo: "https://github.com/avik-singha/good-news",
       alt: "Good News app landing page",
+      tags: [
+        { purple: "Teamwork" },
+        { gray: "Not live" },
+        { blue: "Hackathon" },
+      ],
     },
     {
       id: "3",
       name: "Star-Card",
+      description:
+        " Web platform for real-time reporting and visibility into various workplace risks and hazards.",
       image: starCard,
       live: "https://star-card.us-south.cf.appdomain.cloud/",
       repo: "https://github.com/Fiewor/star-card",
       alt: "Star Card landing page",
-      // https://github.com/Fiewor/star-card
+      tags: [{ purple: "Teamwork" }, { green: "Live" }, { blue: "Hackathon" }],
+    },
+    {
+      id: "4",
+      name: "AnyLease",
+      description:
+        "Web application to help automate leasing. Final team project after virtual internship.",
+      image: anyLease,
+      live: "https://any-lease.netlify.app/",
+      repo: "https://github.com",
+      alt: "Anylease landing page",
+      tags: [{ purple: "Teamwork" }, { green: "Live" }],
     },
   ];
 
   return (
     <>
-      <Grid className="project__grid">
-        {projects.map(({ id, name, image, live, repo, alt }) => {
-          return (
-            <ProjectTile
-              key={id}
-              name={name}
-              image={image}
-              live={live}
-              repo={repo}
-              alt={alt}
-            />
-          );
-        })}
+      <Grid>
+        {/* <Grid className="project__grid"> */}
+        {projects.map(
+          ({ id, name, description, image, live, repo, alt, tags }) => {
+            return (
+              <ProjectTile
+                key={id}
+                name={name}
+                description={description}
+                image={image}
+                live={live}
+                repo={repo}
+                alt={alt}
+                tags={tags}
+              />
+            );
+          }
+        )}
       </Grid>
       <Button
         href="https://github.com/Fiewor"
--- src/pages/ProjectPage/ProjectTile.js
+++ src/pages/ProjectPage/ProjectTile.js
@@ -1,24 +1,52 @@
 import React from "react";
-import { Grid, Column, Link } from "@carbon/react";
+import { Grid, Column, Link, Tag } from "@carbon/react";
 
-const ProjectTile = ({ name, image, live, repo, alt }) => {
+const ProjectTile = ({ name, description, image, live, repo, alt, tags }) => {
   return (
-    <Column md={8} lg={8} sm={4} className="project-tile__column">
-      <p className="project-tile__title">{name}</p>
-      <img className="project-tile-image" src={image} alt={alt} href={repo} />
-      <Grid className="project-tile__grid">
-        <Column md={2} lg={4} sm={2}>
-          <Link href={live} target="_blank" rel="noopener noreferrer">
-            Demo
-          </Link>
-        </Column>
-        <Column md={2} lg={4} sm={2}>
-          <Link href={repo} target="_blank" rel="noopener noreferrer">
-            Repo
-          </Link>
-        </Column>
-      </Grid>
-    </Column>
+    <>
+      {/* <Grid className="project-tile__grid"> */}
+      <Column md={8} lg={8} sm={4} className="project-tile__column">
+        <Grid className="project-tile__header">
+          <Column md={4} lg={4} sm={4}>
+            <p className="project-tile__title">{name}</p>
+          </Column>
+
+          <Column md={4} lg={4} sm={4} className="project-tile__tag-container">
+            {tags.map((tag) => {
+              const [[key, value] = []] = Object.entries(tag);
+              return (
+                <Tag
+                  className="project-tile__tag"
+                  type={key}
+                  size="sm"
+                  title="Clear Filter"
+                  //  {...args}
+                >
+                  {value}
+                </Tag>
+              );
+            })}
+          </Column>
+        </Grid>
+
+        <p className="project-tile__description">{description}</p>
+
+        <img className="project-tile-image" src={image} alt={alt} href={repo} />
+
+        <Grid>
+          <Column md={2} lg={4} sm={2}>
+            <Link href={live} target="_blank" rel="noopener noreferrer">
+              Demo
+            </Link>
+          </Column>
+          <Column md={2} lg={4} sm={2}>
+            <Link href={repo} target="_blank" rel="noopener noreferrer">
+              Repo
+            </Link>
+          </Column>
+        </Grid>
+      </Column>
+    </>
   );
 };
 
