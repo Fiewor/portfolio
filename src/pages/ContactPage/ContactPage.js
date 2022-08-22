65a4d31a60cd9437f1e1deb14b3d05c29cb1aa58
Fiewor <johnfiewor@gmail.com>
2022-08-22 03:01:48 +0100

    feat: add form and messaging logic

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Column,
+  Form,
   FormGroup,
   Stack,
   TextInput,
@@ -9,30 +10,90 @@ import {
   Button,
 } from "@carbon/react";
 import { SendAlt } from "@carbon/react/icons";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
-  const onSubmit = () => {};
+  const [messageDetails, setMessageDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const { firstName, lastName, email, message } = messageDetails;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMessageDetails({ ...messageDetails, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      REACT_APP_EMAILJS_TEMPLATE_ID: templateId,
+      REACT_APP_EMAILJS_SERVICE_ID: serviceId,
+      REACT_APP_EMAILJS_PUBLIC_KEY: publicKey,
+    } = process.env;
+
+    const templateParams = {
+      firstName,
+      lastName,
+      email,
+      message,
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      (err) => {
+        console.log("FAILED...", err);
+      }
+    );
+  };
+
   return (
     <Grid>
       <Column lg={16} md={8} sm={4}>
-        <p>Get in touch...</p>
-        <FormGroup
-          style={{ maxWidth: "500px" }}
-          // {...args}
+        <p className="contact__text">
+          Seen something you like? Or just wanna chat? Shoot me a message!
+        </p>
+        <Form
+          className="contact__form"
+          // {...args}awgd\+
         >
           <Stack gap={7}>
-            <TextInput id="one" labelText="First Name (optional)" />
-            <TextInput id="two" labelText="Last Name (optional)" />
-            <TextInput id="two" labelText="Email (optional)" />
+            <TextInput
+              name="firstName"
+              labelText="First Name (optional)"
+              onChange={handleChange}
+              value={firstName}
+            />
+            <TextInput
+              name="lastName"
+              labelText="Last Name (optional)"
+              onChange={handleChange}
+              value={lastName}
+            />
+            <TextInput
+              name="email"
+              labelText="Email (optional)"
+              onChange={handleChange}
+              value={email}
+            />
             <TextArea
               labelText="Message"
+              name="message"
+              onChange={handleChange}
+              value={message}
+              required
               // {...textareaProps}
             />
-            <Button onClick={onSubmit} renderIcon={SendAlt}>
-              Submit
+            <Button type="submit" onClick={handleSubmit} renderIcon={SendAlt}>
+              Send
             </Button>
           </Stack>
-        </FormGroup>
+        </Form>
       </Column>
     </Grid>
   );
