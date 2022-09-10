7180bdb683d56aa06aa9e1158a2fd4f562ed7bca
Fiewor <johnfiewor@gmail.com>
2022-09-10 13:46:58 +0100

    style: text and spacing changes

--- src/pages/ServicesPage/ServicesPage.js
+++ src/pages/ServicesPage/ServicesPage.js
@@ -36,12 +36,13 @@ const ServicesPage = () => {
               or a short message on my <Link to="/contact">contact page</Link>.
             </p>
             <Accordion className="accordion">
-              <AccordionItem title="Want to know what stack I work with or hear about my past projects? Read more">
+              <AccordionItem title="Have questions about my stack and past projects? Read more">
                 <p>
                   <span className="accordion-span">Stack:</span> React,
-                  JavaScript, TypeScript, Storybook.js, CSS, SASS, jQuery, EJS
+                  JavaScript, TypeScript, Redux, Storybook.js, CSS, SASS, Styled
+                  Components, jQuery, EJS.
                 </p>
-                <p>
+                <p className="detail">
                   See projects <Link to="/projects">here</Link>
                 </p>
               </AccordionItem>
@@ -65,7 +66,7 @@ const ServicesPage = () => {
               <AccordionItem title="Read more">
                 <p>
                   <span className="accordion-span">Stack:</span> Node.js,
-                  MongoDB, Serverless Functions, REST Architecture
+                  MongoDB, Mongoose, Serverless Functions, REST Architecture.
                 </p>
               </AccordionItem>
             </Accordion>
@@ -80,14 +81,17 @@ const ServicesPage = () => {
             </p>
             <p className="detail">
               Upgrade your app to understand natural language and respond
-              intuitively to users text input?
+              intuitively to users' text input?
             </p>
             <p className="detail">
               Extract key phrases and sentiment from text data?
             </p>
             <p className="detail">
-              Embed a bot into your app or upgrade your app's existing search
-              functionality with cognitive superpowers?
+              Embed a conversational bot into your communication channel?
+            </p>
+            <p className="detail">
+              Upgrade your app's search functionality with cognitive
+              superpowers?
             </p>
             <p className="detail-contact">
               Contact me via{" "}
@@ -103,10 +107,28 @@ const ServicesPage = () => {
             </p>
             <Accordion className="accordion">
               <AccordionItem title="Read more">
-                <p>
-                  Microsoft Azure provides a suite of artificial intelligence
-                  services which can be embedded into web applications to bring
-                  a host of cognitive capabilities to these applications.
+                <p className="detail">
+                  Microsoft Azure provides a suite of{" "}
+                  <CarbonLink
+                    href="https://azure.microsoft.com/en-us/services/cognitive-services/#overview"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    artificial intelligence services
+                  </CarbonLink>{" "}
+                  around Language, Speech and Vision which can be embedded into
+                  web applications to bring a host of cognitive capabilities to
+                  these applications.
+                </p>
+                <p className="detail">
+                  <CarbonLink
+                    href="https://docs.microsoft.com/en-us/azure/cognitive-services/what-are-cognitive-services"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Read more
+                  </CarbonLink>{" "}
+                  about Azure's AI services.
                 </p>
                 <p>
                   As a certified Azure{" "}
@@ -117,10 +139,8 @@ const ServicesPage = () => {
                   >
                     AI-Engineer
                   </CarbonLink>
-                  , I help business build web applications embedded with
-                  cognitive services or RESTful APIs utilizing Azure's
-                  artificial intelligence solutions to meet their custom
-                  business needs.
+                  , I help businesses build complete end-to-end AI solutions to
+                  meet custom business needs.
                 </p>
               </AccordionItem>
             </Accordion>
